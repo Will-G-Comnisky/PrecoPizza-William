233635dcd38857ad92dc586d0ab71d331482dcaa
William <william.gispiela@gmail.com>
2023-11-04 19:51:24 -0300

    script JS finalizado, necessário estilizar

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-// Primeiro, uma função simples ao botão de limpar, para resetar o formulário:
+// Primeiro, uma função simples ao botão de limpar formulário, para resetar o formulário:
 document.getElementById('idLimpar').addEventListener('click', function() {
     document.getElementById('idNome').value = '';
     document.getElementById('idTamanho').value = '';
     document.getElementById('idPreco').value = '';
+});
+
+// E uma função ao botão de limpar tabela, para resetar a tabela de pizzas:
+document.getElementById('idLimparTabela').addEventListener('click', function() {
+    let tabela = document.querySelector('.pizzas-table');
+    tabela.innerHTML = '';
 });
 
 // Temos que prever a possível tentativa do usuário colocar valores como 0 para o tamanho ou preço, nesse caso:
@@ -29,6 +35,17 @@ function validarFormulario() {
     return validarTamanho() && validarPreco();
 }
 
+var pizzas = [];
+var melhorCB = null; // Inicializando o melhorCB como nulo
+
+// Função para calcular a diferença percentual e atualizar os dados
+function atualizarDiferencaPercentual(pizza) {
+    
+
+}
+
+// Evento listener que ocorre ao clicar no botão registrar:
+
 var registrar = document.getElementById('idRegistrar');
 registrar.addEventListener('click', function(e) {
     e.preventDefault();
@@ -37,46 +54,33 @@ registrar.addEventListener('click', function(e) {
     if (!validarFormulario()) {
         return;
     }
-
+    
     let nome = document.getElementById('idNome').value;
     let tamanho = parseFloat(document.getElementById('idTamanho').value);
-    let preco = parseFloat(document.getElementById('idPreco').value);
-    let pizzas = [];
+    let preco = parseFloat(document.getElementById('idPreco').value).toFixed(2);
     // Primeiro, ao registrar a pizza, cria-se as variaveis para fazerem o cálculo da área e preço por cm²
     let raio = tamanho / 2;
     let area = Math.PI * Math.pow(raio, 2);
-    let precoPorCm2 = preco / area;
-    // Calculos feitos, agora adicionar ao array
-    pizzas.push([nome, tamanho, preco, precoPorCm2, 0])
-
-    /*
-    var melhorCB = pizzas[0];
-    for (var i = 1; i < pizzas.length; i++) {
-        if (pizzas[i][3] > melhorCB[3]) {
-            melhorCB = pizzas[i];
-        }
-    }
-    */ 
-    /* 
+    let precoPorCm2 = (preco / area).toFixed(3);
+    
+    let pizza = [nome, tamanho, preco, precoPorCm2, 'Melhor custo benefício!' ];
+    
+    pizzas.push(pizza)
+    
     if (pizzas.length > 1) {
         var melhorCB = pizzas.reduce(function (prev, current) {
             return (prev[3] < current[3]) ? prev : current;
         });
-    };   
-    */
-    let melhorCB = pizzas.sort((a, b) => (a.preco / a.precoPorCm2) - (b.preco / b.precoPorCm2))[0];
-
-    // Ordenamos pelo custo benefício, mas agora precisamos calcular a diferença (em porcentagem):
-
-    function calcularDiferencaPercentual(pizzas, melhorCB) {
-        // Calcula a diferença de cada pizza em relação à melhor pizza
+        
         pizzas.forEach(function (pizza) {
-            pizza.diferencaPercentual = ((pizza.preco - melhorCB.preco) / melhorCB.preco) * 100;
-        });   
-        return pizzas;
+            var diferencaPercentual = ((pizza[3] - melhorCB[3]) / melhorCB[3]) * 100;
+            if (diferencaPercentual === 0) {
+                pizza[4] = 'Melhor custo benefício!'
+            } else {
+                pizza[4] = '+' + diferencaPercentual.toFixed(2) + '%';
+            }
+        });
     }
-
-    pizzas = calcularDiferencaPercentual(pizzas, melhorCB);
     atualizarTabela(pizzas);
     return pizzas;
 });
@@ -84,26 +88,63 @@ registrar.addEventListener('click', function(e) {
 // Com a lógica de cálculos feita, precisamos criar a tabela que sairá como resultado na tela:
 
 function atualizarTabela(dados) {
-    let container = document.querySelector('.pizzas');
-    // Selecionamos o array de pizzas
-
-    // Para isso, precisamos criar uma tabela com os dados
-    let tabela = document.createElement('table');
-    tabela.classList.add('pizzas-table');
-
-    // Percorre o array de pizzas e cria uma nova linha para cada pizza adicionada
-    dados.forEach(function (pizza) {
-        let linha = document.createElement('tr');
-
-        // Adiciona as colunas à linha criada
-        for (let coluna in pizza) {
-            let celula = document.createElement('td');
-            celula.textContent = pizza[coluna];
-            linha.appendChild(celula);
-        }
-
-        // Adiciona a linha à tabela, e a tabela ao container, finalizando o processo
-        tabela.appendChild(linha);
-    });
-    container.appendChild(tabela);
+    // Seleciona o elemento da tabela
+    let tabela = document.querySelector('.pizzas-table');
+    
+    // Se o elemento da tabela existir, atualiza-o
+    if (tabela) {
+        tabela.innerHTML = '';
+        
+        // Ordena os dados com base no "Preço por cm²"
+        dados.sort((a, b) => a[3] - b[3]);
+        
+        // Percorre o array de pizzas e cria uma nova linha para cada pizza adicionada
+        dados.forEach(function (pizza) {
+            var linha = document.createElement('tr');
+            
+            // Adiciona as colunas à linha criada
+            for (let i = 0; i < pizza.length; i++) {
+                let celula = document.createElement('td');
+                celula.textContent = pizza[i];
+                linha.appendChild(celula);
+            }
+            
+            // Adiciona a linha à tabela
+            tabela.appendChild(linha);
+        });
+    }
 }
+
+
+/*
+TRECHO DE CÓDIGOS QUE ESTAVAM SENDO USADOS MAS FORAM ALTERADOS:
+var melhorCB = pizzas[0];
+for (var i = 1; i < pizzas.length; i++) {
+    if (pizzas[i][3] > melhorCB[3]) {
+        melhorCB = pizzas[i];
+    }
+}
+*/ 
+/* 
+if (pizzas.length > 1) {
+    var melhorCB = pizzas.reduce(function (prev, current) {
+        return (prev[3] < current[3]) ? prev : current;
+    });
+};   
+
+
+
+let melhorCB = pizzas.sort((a, b) => (a.preco / a.precoPorCm2) - (b.preco / b.precoPorCm2))[0];
+
+// Ordenamos pelo custo benefício, mas agora precisamos calcular a diferença (em porcentagem):
+
+function calcularDiferencaPercentual(pizzas, melhorCB) {
+    // Calcula a diferença de cada pizza em relação à melhor pizza
+    pizzas.forEach(function (pizza) {
+        pizza.diferencaPercentual = ((pizza.preco - melhorCB.preco) / melhorCB.preco) * 100;
+    });   
+    return pizzas;
+}
+
+pizzas = calcularDiferencaPercentual(pizzas, melhorCB);
+*/
